c8dcc7fa0f1c68beb0f2a458a6837619bf319f3e
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2023-09-18 11:20:08 -0400

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: 'export',
+}
 
 module.exports = nextConfig
