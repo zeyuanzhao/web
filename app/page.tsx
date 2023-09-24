3db994713a6ec91192f901806a41a88db5cceca1
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2023-09-24 18:57:53 -0400

    home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <>
-      <h1 className="text-2xl">Home</h1>
-      <p></p>
-    </>
+    <main className="mt-10 mx-16">
+      <h1 className="text-5xl mb-4">Home</h1>
+      <img src="" alt="" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut iaculis tellus. Ut eget est sodales, pretium tortor ullamcorper, porta nisl. Fusce consectetur turpis at nibh pharetra aliquam a vel lectus. Vestibulum eget euismod quam. Mauris nisi lorem, maximus eu vehicula vitae, fermentum non dui. Cras tincidunt elit ac varius consequat. Ut pellentesque suscipit dolor at tempor. Fusce at mi id nunc varius convallis. Sed mollis urna in vulputate euismod. Phasellus in malesuada ligula.</p>
+    </main>
   );
 }
