4afd28dcd3732f713db881685a9a0b28306d42a5
azhaodev <14340342+zeyuanzhao@users.noreply.github.com>
2024-01-26 04:13:10 +0000

    github

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,38 +1,48 @@
 import Image from "next/image";
-import githubLogo from "../../public/github-mark-white.svg";
+import githubLogoDark from "../../public/github-mark-white.svg";
+import githubLogoLight from "../../public/github-mark.svg";
+import Link from "next/link";
 
 export const Navbar = () => {
   return (
     <nav>
       <div className="py-4 align-middle">
         <h1 className="inline-block text-3xl font-bold ml-8 align-middle">
-          <a href="/">Alex Zhao</a>
+          <Link href="/">Alex Zhao</Link>
         </h1>
         <ul className="inline-block list-none text-2xl ml-8 align-middle">
           <li className="inline-block mx-8">
-            <a href="/" className="hover:underline">
+            <Link href="/" className="hover:underline">
               Home
-            </a>
+            </Link>
           </li>
           <li className="inline-block mx-8">
-            <a href="/about" className="hover:underline">
+            <Link href="/about" className="hover:underline">
               About
-            </a>
+            </Link>
           </li>
           <li className="inline-block mx-8">
-            <a href="/projects" className="hover:underline">
+            <Link href="/projects" className="hover:underline">
               Projects
-            </a>
+            </Link>
           </li>
           <li className="inline-block mx-8">
-            <a href="/recordings" className="hover:underline">
+            <Link href="/recordings" className="hover:underline">
               Recordings
-            </a>
+            </Link>
           </li>
         </ul>
-        <ul className="inline-block list-none align-middle">
-          <li>
-            <Image src={githubLogo} width="35" alt="GitHub" />
+        <ul className="inline-block list-none align-middle float-right mr-8">
+          <li className="none dark:block">
+            <Link href="https://github.com/zeyuanzhao" target="_blank">
+              <picture>
+                <source
+                  srcSet={githubLogoDark.src}
+                  media="(prefers-color-scheme: dark)"
+                />
+                <Image src={githubLogoLight} width="35" alt="GitHub" />
+              </picture>
+            </Link>
           </li>
         </ul>
       </div>
