0294648e9ea57cbec77ac7c2c0640b3bba003eaf
azhaodev <azhaodev@gmail.com>
2024-01-27 18:46:58 +0000

    footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer>
-      
+    <footer className="bg-slate-100 fixed bottom-0 left-0 w-full p-4 border-t border-black-300">
+      <p className="inline-block">Copyright © 2024 Alex Zhao</p>
+      <p className="inline-block float-right">Source code: <Link href="https://github.com/zeyuanzhao/web" className="underline">GitHub</Link></p>
     </footer>
   );
 }
