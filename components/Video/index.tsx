130700e44bbfe2ea57e80d5d1b586d006385ef20
azhaodev <14340342+zeyuanzhao@users.noreply.github.com>
2024-01-30 20:14:02 +0000

    embed fix

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -9,7 +9,7 @@ const Video = ({
 }) => {
   return (
     <div className="rounded-xl overflow-hidden border-white shadow-lg shadow-gray-300">
-      <iframe width={width} height={height} src={src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width={width} height={height} src={src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   )
 };
